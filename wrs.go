518add31b0fa6d01e8554b43ed8bb983e0576725
Keftcha <alban.dewilde@gmail.com>
2020-02-29 03:23:56 +0100

    Give the sound name in the query string as `song`

--- wrs.go
+++ wrs.go
@@ -7,11 +7,18 @@ import (
 )
 
 func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	song := r.URL.Query().Get("song")
+	if song != "" {
+		fmt.Fprintf(w, "Currently playing: %s!", song)
+		fmt.Println("Listening: ", song)
+	} else {
+		fmt.Fprint(w, "No song is playing.")
+		fmt.Println("Not listening")
+	}
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-	http.HandleFunc("/", root)
+	fmt.Println("The server is now running !")
+	http.HandleFunc("/hoster", root)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
