85b6dddad45bc59a1b14b1e6a6c3063b816f8a45
Keftcha <alban.dewilde@gmail.com>
2020-03-05 14:07:54 +0100

    Make the page for user to acces and liste all our music files
    In the code I also made function to reuse the code
    the hoster page is the page of the streamer
    and the page where to listen is the listen page

--- wrs.go
+++ wrs.go
@@ -18,12 +18,13 @@ type SongLink struct {
 }
 
 type Markers struct {
-	Title      string
-	SongPath   string
-	SongType   string
-	FilesLinks []SongLink
-	Username   string
-	Password   string
+	Title         string
+	SongPath      string
+	SongType      string
+	FilesLinks    []SongLink
+	Username      string
+	Password      string
+	Authenticated bool
 }
 
 func checkCredentials(username, password string) bool {
@@ -46,7 +47,7 @@ func loadDirectoryTree(root string) []string {
 	return files
 }
 
-func listen(w http.ResponseWriter, r *http.Request) {
+func stream(w http.ResponseWriter, r *http.Request) {
 	song := r.URL.Query().Get("song")
 
 	// Check if the user is authentificated
@@ -61,40 +62,58 @@ func listen(w http.ResponseWriter, r *http.Request) {
 
 	// Our markers
 	markers := new(Markers)
+	markers.Authenticated = true
 
 	// Fill markers values
-	if song == "" { // The song parameter isn't present
-		markers.Title = "No song playing"
-		fmt.Println("Not listening")
-	} else { // The song parameter is present
-		markers.SongPath = fmt.Sprintf("/music/%s", song)
-
-		// Check if we can acces to the file
-		if _, err := os.Stat(markers.SongPath); err != nil { // We can't acces the file
-			markers.Title = "Error finding file"
-			fmt.Println(err)
-		} else { // We can acces the file
-			// get the mime type of the song
-			var mimeType string
-			if mime, err := mimetype.DetectFile(markers.SongPath); err == nil {
-				mimeType = mime.String()
-			} else {
-				fmt.Println(err)
-				mimeType = "application/octet-stream"
-			}
-
-			markers.Title = song
-			markers.SongType = mimeType
-			fmt.Println("Listening: ", markers.Title)
-		}
-	}
+	markers.Title, markers.SongType, markers.SongPath = findSong(song)
 
 	// Load directoies and files
 	var files []string = loadDirectoryTree("/music")
 
+	// Make the list of song
+	linkTpl := "/hoster/?song=%s"
+	markers.FilesLinks = makeSongsLink(files, linkTpl)
+	// Add the authentigication parameters to song links
+	authTpl := "&username=%s&password=%s"
+	for idx, songLink := range markers.FilesLinks {
+		markers.FilesLinks[idx].Link = songLink.Link + fmt.Sprintf(authTpl, os.Getenv("username"), os.Getenv("password"))
+	}
+
+	// Load and execute the template
+	tpl, _ := template.ParseFiles("page/player.html")
+	tpl.Execute(w, markers)
+}
+
+func listen(w http.ResponseWriter, r *http.Request) {
+	song := r.URL.Query().Get("song")
+
+	// Our markers
+	markers := new(Markers)
+	markers.Authenticated = false
+
+	// Fill markers values
+	markers.Title, markers.SongType, markers.SongPath = findSong(song)
+
+	// Load directoies and files
+	var files []string = loadDirectoryTree("/music")
+
+	// Make the list of song
+	linkTpl := "/listen/?song=%s"
+	markers.FilesLinks = makeSongsLink(files, linkTpl)
+
+	// Load and execute the template
+	tpl, _ := template.ParseFiles("page/player.html")
+	tpl.Execute(w, markers)
+}
+
+func signin(w http.ResponseWriter, r *http.Request) {
+	tpl, _ := template.ParseFiles("page/sign_in.html")
+	tpl.Execute(w, struct{}{})
+}
+
+func makeSongsLink(files []string, linkTpl string) []SongLink {
 	// Remove the `/music/` part of files path and remove all non audio files
 	audioFiles := make([]SongLink, 0)
-	audioLinkTpl := "/hoster/?song=%s&username=%s&password=%s"
 	for _, path := range files {
 		mt, err := mimetype.DetectFile(path)
 		mime := strings.Split(mt.String(), "/")[0] // General type of file
@@ -103,37 +122,57 @@ func listen(w http.ResponseWriter, r *http.Request) {
 			// Build the song url link
 			audioTitle := path[len("/music/"):]
 			audioLink := fmt.Sprintf(
-				audioLinkTpl,
+				linkTpl,
 				audioTitle,
-				os.Getenv("username"),
-				os.Getenv("password"),
 			)
 
 			songLink := SongLink{Title: audioTitle, Link: audioLink}
 			audioFiles = append(audioFiles, songLink)
 		}
 	}
-
-	markers.FilesLinks = audioFiles
-
-	// Load and execute the template
-	tpl, _ := template.ParseFiles("page/player.html")
-	tpl.Execute(w, markers)
+	return audioFiles
 }
 
-func sign_in(w http.ResponseWriter, r *http.Request) {
-	tpl, _ := template.ParseFiles("page/sign_in.html")
-	tpl.Execute(w, struct{}{})
+func findSong(song string) (title string, songType string, songPath string) {
+	if song == "" { // The song parameter isn't present
+		title = "No song playing"
+		fmt.Println("Not listening")
+	} else { // The song parameter is present
+		songPath = fmt.Sprintf("/music/%s", song)
+
+		// Check if we can acces to the file
+		if _, err := os.Stat(songPath); err != nil { // We can't acces the file
+			title = "Error finding file"
+			fmt.Println(err)
+		} else { // We can acces the file
+			// get the mime type of the song
+			var mimeType string
+			if mime, err := mimetype.DetectFile(songPath); err == nil {
+				mimeType = mime.String()
+			} else {
+				fmt.Println(err)
+				mimeType = "application/octet-stream"
+			}
+
+			title = song
+			songType = mimeType
+			fmt.Println("Listening: ", title)
+		}
+	}
+	return title, songType, songPath
 }
 
 func main() {
 	fmt.Println("The server is now running !")
 
-	http.HandleFunc("/hoster", listen)
-	http.HandleFunc("/hoster/", listen)
+	http.HandleFunc("/hoster", stream)
+	http.HandleFunc("/hoster/", stream)
 
-	http.HandleFunc("/signin", sign_in)
-	http.HandleFunc("/signin/", sign_in)
+	http.HandleFunc("/signin", signin)
+	http.HandleFunc("/signin/", signin)
+
+	http.HandleFunc("/listen", listen)
+	http.HandleFunc("/listen/", listen)
 
 	// Serve sound as static file when path start with `/music/`
 	http.Handle(
